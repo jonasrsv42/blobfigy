9165bb6798509dfa19bcbb6157e0a6ba17b623ae
jonasrsv42 <jonasrsv42@gmail.com>
2026-08-08 13:46:07 +0200

    Whee, good night.</p>

--- src/types/value.rs
+++ src/types/value.rs
@@ -2,7 +2,7 @@
 
 use super::array::PyArray;
 use super::file::PyFile;
-use blobfig::{Array, DType, File, Value, ValueView};
+use blobfig::{Array, DType, File, List, Object, ObjectNode, Value, ValueView};
 use numpy::{PyArray1, PyArrayMethods, PyUntypedArrayMethods};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
@@ -110,7 +110,7 @@ pub fn py_to_value(obj: &Bound<'_, PyAny>) -> PyResult<Value> {
             let val = py_to_value(&v)?;
             entries.push((key, val));
         }
-        return Ok(Value::Object(entries));
+        return Ok(Value::Object(Object::new(entries)));
     }
 
     // Check for list -> List
@@ -119,7 +119,7 @@ pub fn py_to_value(obj: &Bound<'_, PyAny>) -> PyResult<Value> {
         for item in l.iter() {
             items.push(py_to_value(&item)?);
         }
-        return Ok(Value::List(items));
+        return Ok(Value::List(List::new(items)));
     }
 
     Err(PyValueError::new_err(format!(
@@ -152,8 +152,8 @@ pub fn value_to_py<'py>(py: Python<'py>, view: &ValueView<'_>) -> PyResult<Bound
         }
         ValueView::Object(entries) => {
             let dict = PyDict::new(py);
-            for (k, v) in entries.iter() {
-                dict.set_item(*k, value_to_py(py, v)?)?;
+            for (k, v) in entries.entries() {
+                dict.set_item(k, value_to_py(py, v)?)?;
             }
             Ok(dict.into_any())
         }
